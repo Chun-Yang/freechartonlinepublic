fcc52e13d17db2267ea9d39c77874b1df7460be5
yang2007chun <yang2007chun@fb.com>
2019-02-23 17:38:30 -0800

    update

--- build/precache-manifest.d7cf7eb81cc372d2fa807a22e8d4b543.js
+++ build/precache-manifest.d7cf7eb81cc372d2fa807a22e8d4b543.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "c0deeb3febf200e76aeb",
-    "url": "/static/js/main.c0deeb3f.chunk.js"
+    "revision": "73c1d0ada318feaa8bb1",
+    "url": "/static/js/main.73c1d0ad.chunk.js"
   },
   {
     "revision": "d1ff63ddf5f7ba5ae4fb",
     "url": "/static/js/1.d1ff63dd.chunk.js"
   },
   {
-    "revision": "c0deeb3febf200e76aeb",
-    "url": "/static/css/main.209019c2.chunk.css"
+    "revision": "73c1d0ada318feaa8bb1",
+    "url": "/static/css/main.4bb88645.chunk.css"
   },
   {
     "revision": "d1ff63ddf5f7ba5ae4fb",
     "url": "/static/css/1.6f932b95.chunk.css"
   },
   {
-    "revision": "b43ef2e2e2c8ef1040c9606f26acd552",
+    "revision": "37af78ec3575f9f87e1c8d836f43d36e",
     "url": "/index.html"
   }
 ];
